a2ab6ea52eb67f184d56eccd339e72e250fa2283
ggionssi <jeonssi2672@gmail.com>
2025-03-14 21:44:30 +0900

    (20250314)데이터베이스 설치

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using PlayFab;
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Succeed,
+            Fail
+        );
+    }
+
+    public void Succeed(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Lobby");
+    }
+
+    public void Fail(PlayFabError playFabEroor)
+    {
+        Debug.Log(playFabEroor.ToString());
+    }
+}
